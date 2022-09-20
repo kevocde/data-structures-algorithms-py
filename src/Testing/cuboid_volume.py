57ada51ebc16f28ab2603d3bcc962b948845b6b3
Kevin Guzman (kevocde) <kevocde@gmail.com>
2022-09-20 17:46:08 -0500

    [added] nuevo caso de testeo para archivo de testing

--- src/Testing/cuboid_volume.py
+++ src/Testing/cuboid_volume.py
@@ -1,2 +1,5 @@
 def cuboid_volume(length):
+    if type(length) not in [int, float]:
+        raise TypeError
+
     return length**3
--- src/Testing/unit/test_cuboid_volume.py
+++ src/Testing/unit/test_cuboid_volume.py
@@ -7,8 +7,10 @@ class TestCuboidVolume(unittest.TestCase):
         self.assertAlmostEqual(cuboid_volume(2), 8)
         self.assertAlmostEqual(cuboid_volume(1), 1)
         self.assertAlmostEqual(cuboid_volume(0), 0)
-        self.assertAlmostEqual(cuboid_volume(5.5), 166.375)
+
+    def test_input_value(self):
+        self.assertRaises(TypeError, cuboid_volume, True)
 
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(argv=[''], verbosity=2, exit=False)
